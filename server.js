482c024dcf7c2226f595dab6b2736d0167867ccd
shankey28 <shashiishankar@gmail.com>
2015-12-31 11:48:18 -0500

    First node app code

---
+++ server.js
@@ -0,0 +1,10 @@
+var http=require('http');
+var fs=require('fs');
+
+http.createServer(function(req,res){
+    fs.readFile('index.html',function(err,data){
+        res.writeHead(200,{'Content-type':'text/html'});
+        res.end(data);
+        });
+    }).listen(1337,'127.0.0.1');
+console.log("Web Server Started");
